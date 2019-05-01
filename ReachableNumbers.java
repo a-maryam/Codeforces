1e3ab0b4b24ae893af38796944192a409c766147
Aleena <aleenamaryam@Aleenas-MacBook-Pro.local>
2019-05-01 11:57:54 -0400

    Added ReachableNumbers.

---
+++ ReachableNumbers.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+public class ReachableNumbers {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        HashSet<Integer> mod = new HashSet<Integer>();
+        mod.add(n);
+        int x = apply_f(n);
+        while (mod.contains(x) == false) {
+            mod.add(x);
+            n = x;
+            x = apply_f(n);
+        }
+        System.out.println(mod.size());
+        scan.close();
+    }
+
+    public static int apply_f(int n) {
+        n++;
+        while (n % 10 == 0) {
+            n = n / 10;
+        }
+        return n;
+    }
+}
